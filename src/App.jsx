fb1b36437dd2c473df715ef9284f032c3295534a
kapilk34 <kapilkumargupta1511@gmail.com>
2026-04-25 21:33:18 +0530

    fixing the server error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 
-export const ServerUrl = "http://localhost:8000"
+export const ServerUrl = "http://localhost:5000"
 function App() {
   return (
     <Routes>
